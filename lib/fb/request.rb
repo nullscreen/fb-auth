b670b613f5770f12fbcfb992c2e6d9b5a898ffd0
Aaron Dao <hdao61@gmail.com>
2017-06-23 10:38:27 -0700

    Refactor: moved code into new Fb::Request class.
    Removed potential error message in getting the long-term token.

---
+++ lib/fb/request.rb
@@ -0,0 +1,36 @@
+require 'uri'
+require 'net/http'
+require 'json'
+require 'fb/error'
+
+module Fb
+  # @private
+  class Request
+    def initialize(options = {})
+      @host = options.fetch :host, 'graph.facebook.com'
+      @path = options[:path]
+      @params = options.fetch :params, {}
+      @params.merge!(client_id: ENV['FB_CLIENT_ID'])
+    end
+
+    def url
+      uri.to_s
+    end
+
+    def run
+      res = Net::HTTP.get_response(uri)
+      unless res.is_a?(Net::HTTPSuccess)
+        message = JSON.parse(res.body)["error"]["message"]
+        raise Fb::Error, message
+      end
+      JSON.parse(res.body)
+    end
+
+  private
+
+    def uri
+      query = URI.encode_www_form @params
+      URI::HTTPS.build host: @host, path: @path, query: query
+    end
+  end
+end
